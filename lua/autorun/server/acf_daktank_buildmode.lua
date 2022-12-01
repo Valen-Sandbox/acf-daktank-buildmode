7f7bea57ef29f6299f9c05852a5843ac1319f31b
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2022-12-01 11:35:38 -0500

    Initial file upload

---
+++ lua/autorun/server/acf_daktank_buildmode.lua
@@ -0,0 +1,25 @@
+local IsValid = IsValid
+local GetNWBool = GetNWBool
+
+hook.Add("ACF_PreDamageEntity", "ACF_BuildmodeIntegration", function(entity, dmgResult, dmgInfo)
+    if not IsValid(entity) then return end
+
+    local entOwner = entity:CPPIGetOwner()
+    local attacker = dmgInfo:GetAttacker()
+    if not IsValid(entOwner and attacker) then return end
+
+    local entOwnerBuild = entOwner:GetNWBool("_Kyle_Buildmode", false)
+    local attackerBuild = attacker:GetNWBool("_Kyle_Buildmode", false)
+    if entOwnerBuild or attackerBuild then return false end
+end)
+
+hook.Add("DakTankDamageCheck", "DakTank_BuildmodeIntegration", function(hitEnt, shellOwner, shell)
+    if not IsValid(hitEnt and shellOwner and shell) then return end
+
+    local entOwner = hitEnt:CPPIGetOwner()
+    if not IsValid(entOwner) then return end
+
+    local entOwnerBuild = entOwner:GetNWBool("_Kyle_Buildmode", false)
+    local attackerBuild = shellOwner:GetNWBool("_Kyle_Buildmode", false)
+    if entOwnerBuild or attackerBuild then return false end
+end)
