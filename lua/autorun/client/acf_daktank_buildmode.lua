9457243cbd691efcf0171558cd2abb1c23f55b1a
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2023-06-26 16:45:28 -0400

    Damage notif cooldown

--- lua/autorun/client/acf_daktank_buildmode.lua
+++ lua/autorun/client/acf_daktank_buildmode.lua
@@ -1,4 +1,10 @@
+local CurTime = CurTime
+
+local notifDelay = CurTime()
+
 net.Receive( "ACF_BuildmodeNotif", function()
+    if notifDelay >= CurTime() then return end
+
     local isACF, dmgType = net.ReadBool(), ""
     if isACF then
         dmgType = "ACF"
@@ -8,4 +14,5 @@ net.Receive( "ACF_BuildmodeNotif", function()
 
     surface.PlaySound( "buttons/button10.wav" )
     notification.AddLegacy( "You are in buildmode and cannot deal " .. dmgType .. " damage!", NOTIFY_ERROR, 2 )
+    notifDelay = CurTime() + 1
 end )
